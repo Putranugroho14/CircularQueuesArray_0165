c4899a121e00e3e74c3915391a046443a672b5d9
Putranugroho14 <putranugroho61@gmail.com>
2024-05-30 10:51:23 +0700

    membuat void remove dan if di dalamnya

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -40,8 +40,12 @@ public:
 
     }
     void remove() {
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
 
-    }
+         }
 
 
 int main()
